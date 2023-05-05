e62a7cb923a681956e3a99d3a22f1598a98a3276
Rayna Miller <89815185+UsernameI23@users.noreply.github.com>
2023-05-05 18:01:05 -0400

    Update Program.cs

--- HashSet/Program.cs
+++ HashSet/Program.cs
@@ -1,43 +1,45 @@
-﻿namespace SortedList
+namespace HashSets
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //creation of sortedlist  
-            SortedList<string, string> bucketList = new SortedList<string, string>();
-            //add the elements in sortedlist  
-            bucketList.Add("Italy", "Venice");
-            bucketList.Add("UK", "England");
-            bucketList.Add("France", "Cannes");
-            bucketList.Add("Greece", "Athens");
-            bucketList.Add("Spain", "Barcelona");
-            //display the elements of the sortedlist  
-            Console.WriteLine("The items in my sorted bucket list are:");
-            foreach (KeyValuePair<string, string> bl in bucketList)
+            HashSet<string> languages = new HashSet<string>();
+            languages.Add("Japanese");
+            languages.Add("Spanish");
+            languages.Add("English");
+            languages.Add("Italian");
+
+            HashSet<string> otherLanguages = new HashSet<string>();
+            otherLanguages.Add("Chinese");
+            otherLanguages.Add("Arabic");
+            otherLanguages.Add("Russain");
+            otherLanguages.Add("French");
+
+            HashSet<string> uncommonLanguages = new HashSet<string>();
+            uncommonLanguages.Add("Tagalog");
+            uncommonLanguages.Add("Egyptian");
+            uncommonLanguages.Add("Latin");
+            uncommonLanguages.Add("Japanese");
+            uncommonLanguages.Add("Italian");
+
+            Console.WriteLine("Combined list of older and newer languages");
+            languages.UnionWith(otherLanguages);
+            foreach (string language in languages)
             {
-                Console.WriteLine($"Key = {bl.Key}  Value={bl.Value}");
+                Console.WriteLine(language);
             }
-            Console.WriteLine("What city would you like to go to?");
-            string city = Console.ReadLine();
-            Console.WriteLine("What country is it in?");
-            string country = Console.ReadLine();
-            //check for value in list
-            if (bucketList.ContainsValue(city))
-                Console.WriteLine($"{city} is one of the values in your list");
-            else
+            Console.WriteLine();
+
+            Console.WriteLine("Languages that are in both lists");
+            HashSet<string> bothLists = new HashSet<string>();
+            bothLists = languages;
+            bothLists.IntersectWith(uncommonLanguages);
+            foreach (string language in bothLists)
             {
-                // check for country key in list (can't have dups)
-                if (bucketList.ContainsKey(country))
-                {
-                    Console.WriteLine($"You can only visit 1 city from {country} ");
-                }
-                else
-                {
-                    bucketList.Add(country, city);
-                    Console.WriteLine("{city} {country} was added to your list");
-                }
+                Console.WriteLine(language);
             }
         }
     }
 }
+
